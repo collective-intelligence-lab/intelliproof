b29b50ba371dc4e04aa101fd424cac83d2927ddc
Kabir Pamnani <kabirgpamnani@gmail.com>
2025-06-04 15:13:56 -0400

    feat: implement graph editor with ReactFlow and add node creation functionality

--- src/app/graph-editor/page.tsx
+++ src/app/graph-editor/page.tsx
@@ -1,15 +1,21 @@
-'use client';
-import React, { useState } from 'react';
-import Header from '../../components/Header';
-import Navbar from '../../components/Navbar';
+"use client";
+import React, { useState } from "react";
+import Header from "../../components/Header";
+import Navbar from "../../components/Navbar";
+import MainLayout from "../../components/Layout/MainLayout";
 
 export default function GraphEditorPage() {
-    const [isNavbarOpen, setNavbarOpen] = useState(false);
-    return (
-        <div className="min-h-screen flex flex-col bg-white">
-            <Header userName="Anesu Gavhera" onMenuClick={() => setNavbarOpen(true)} />
-            <Navbar isOpen={isNavbarOpen} onClose={() => setNavbarOpen(false)} />
-            <div className="flex items-center justify-center flex-1 text-3xl">Graph Editor</div>
-        </div>
-    );
-} 
+  const [isNavbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <div className="min-h-screen flex flex-col bg-white">
+      <Header
+        userName="Anesu Gavhera"
+        onMenuClick={() => setNavbarOpen(true)}
+      />
+      <Navbar isOpen={isNavbarOpen} onClose={() => setNavbarOpen(false)} />
+      <div>
+        <MainLayout />
+      </div>
+    </div>
+  );
+}
